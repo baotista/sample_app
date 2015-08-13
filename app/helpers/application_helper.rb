23a5c82a245173415904b7d017b903b843e75478
Baptiste Goulard <bgoulard@gmail.com>
2015-08-13 17:09:27 +0200

    ajout d'un helper pour le titre

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #retourner un titre basé sur la page
+    def titre
+        base_titre = "Simple App du tuto Ruby on Rails"
+        if @titre.nil?
+            base_titre
+        else
+            "#{base_titre} | #{@titre}"
+        end
+    end
 end
